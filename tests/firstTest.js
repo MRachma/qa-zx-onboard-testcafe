ec9f04d84c42680ba6fdeeb70092f454e56dedf0
MRachma <moch.rachman@zebrax.id>
2022-01-28 00:56:40 +0700

    Chapter3.3

--- tests/firstTest.js
+++
@@ -1,9 +0,0 @@
-fixture("First Fixture Onboard")
-    .page("https://devexpress.github.io/testcafe/example/ ");
-test("First Test", async (t) => {
-    await t
-        .typeText('#developer-name','TAU')
-        .click('input#macos')
-        .click('#submit-button');
-    
-});
