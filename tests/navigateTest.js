5e508aa36e50acb51b6625ca1c9621823ba15255
MRachma <moch.rachman@zebrax.id>
2022-01-28 10:03:03 +0700

    Chapter4.3

--- tests/navigateTest.js
+++ tests/navigateTest.js
@@ -1,20 +1,7 @@
-fixture.meta('version','1')("First Fixture")
-    .page("http://devexpress.github.io/testcafe/");
+fixture('Navigate Example')
+        .page('https://devexpress.github.io/testcafe/example/')
 
-test.meta('env','production')
-.page("https://devexpress.github.io/testcafe/example/")
-("First Test", async t =>{
-    await t
-        .typeText("#developer-name","TAU")
-        .click("#macos")
-        .click("#submit-button");
-});
-
-test
-.page("https://devexpress.github.io/testcafe/example/")
-("Second Test", async t =>{
-    await t
-        .typeText("#developer-name","TAU")
-        .click("#macos")
-        .click("#submit-button");
-});
+        test('Navigate Test', async t => {
+                    await t // navigate to url
+                        .navigateTo("https://google.com/");
+        })
